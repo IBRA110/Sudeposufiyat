a89c3545c3fb2af9c3eeb656b4f8ff32b1ecb72e
Ibrahim <mamedovarbeiter@gmail.com>
2022-07-06 07:14:44 +0300

    header-mobile-ready

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
 window.onload = () => {
+	
 	let burger = document.querySelector('.burger i')
 	let menu = document.querySelector('.list')
-	
+	let listOfLinks = document.querySelectorAll('.link')
+	let listOfA = document.querySelectorAll('.a-link')
+
 	burger.addEventListener('click', () => {
-		if ( menu.style.transform === 'scaleY(1)'){
+		if (menu.style.transform === 'scaleY(1)'){
 			menu.style.transform = 'scaleY(0)';
 		}else{
 			menu.style.transform = 'scaleY(1)';
 		}
-	});	
+	});
+	
+
+	listOfLinks.forEach(function(link){
+		if (link.childNodes.length > 1){
+			link.childNodes[3].addEventListener('click', () => {
+				link.childNodes[5].classList.toggle('show');
+			});
+		}
+	});
+
+
+	listOfA.forEach(function(a){
+		a.addEventListener('click', () => {
+			menu.style.transform = 'scaleY(0)';
+		});
+	});
 };
